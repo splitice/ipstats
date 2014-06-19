ff003010e25502d3e78438cbdc6a51142fb80943
splitice <mat999@gmail.com>
2014-06-19 15:49:48 +1000

    upgraded build script to provide option for pfring build

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-g++ ipstats.c -lpcap -O3 -o ipstats
+COMPILE_OPTIONS="-lpcap"
+
+if [[ $1 == "fring" ]]; then
+	COMPILE_OPTIONS="$LIBS -lpfring -lnuma -DUSE_PF_RING"
+fi
+
+g++ ipstats.c $COMPILE_OPTIONS -O3 -o ipstats
