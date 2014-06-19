0a3510d4168cb7584336e201003535ce8c74a9f4
splitice <mat999@gmail.com>
2014-06-19 16:55:11 +1000

    add missing reference

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 COMPILE_OPTIONS="-lpcap"
 
 if [[ $1 == "pfring" ]]; then
-	COMPILE_OPTIONS="$COMPILE_OPTIONS -lpfring -lnuma -DUSE_PF_RING"
+	COMPILE_OPTIONS="$COMPILE_OPTIONS -lpfring -lnuma -lrt -DUSE_PF_RING"
 fi
 
 g++ ipstats.c $COMPILE_OPTIONS -O3 -o ipstats
