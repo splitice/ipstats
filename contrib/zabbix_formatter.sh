7b0437c9b2afbc152e7b61bb34b6c92b054d12bb
splitice <mat999@gmail.com>
2015-06-14 15:21:24 +1000

    Higher performance for the ipstats contrib script

--- contrib/zabbix_formatter.sh
+++ contrib/zabbix_formatter.sh
@@ -2,47 +2,40 @@
 #Usage: ipstats eth1 | bash contrib/zabbix_formatter.sh SERVER | zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -i -
 
 HOST="$1"
-BUFFER=""
-
-function send_buffer {
-        echo -n "$BUFFER" | /usr/bin/zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -vv -i -
-        BUFFER=""
-}
-
-function value_n {
-        echo "$1" | awk "{print \$$2}"
-}
 
 function get_data_line {
-        KEY=$(echo "$1" | awk "BEGIN {ORS=\" \"} {print \"net.ipstat[\\\"\"\$1\"\\\",\\\"\"\$2\"\\\",\\\"$2\\\"]\"}")
-        VALUE=$(value_n "$line" $3)
-        BUFFER="$BUFFER$HOST $KEY$VALUE"$'\n'
-
+        echo "$1" | awk '{print "'$HOST' net.ipstat[\""$1"\",\""$2"\",\"'$2'\"] "$'$3'}'
 }
 
 while read line; do
-        if [[ "${line:0:1}" != "#" ]]; then
-                get_data_line "$line" "tcp_packets" 3
-                get_data_line "$line" "tcp_bytes" 4
+        run=1
+        while [[ $run ]]; do
+                if [[ "${line:0:1}" != "#" ]]; then
+                        get_data_line "$line" "tcp_packets" 3
+                        get_data_line "$line" "tcp_bytes" 4
 
-                get_data_line "$line" "udp_packets" 5
-                get_data_line "$line" "udp_bytes" 6
+                        get_data_line "$line" "udp_packets" 5
+                        get_data_line "$line" "udp_bytes" 6
 
-                get_data_line "$line" "gre_packets" 7
-                get_data_line "$line" "gre_bytes" 8
+                        get_data_line "$line" "gre_packets" 7
+                        get_data_line "$line" "gre_bytes" 8
 
-                get_data_line "$line" "ipip_packets" 9
-                get_data_line "$line" "ipip_bytes" 10
+                        get_data_line "$line" "ipip_packets" 9
+                        get_data_line "$line" "ipip_bytes" 10
 
-                get_data_line "$line" "icmp_packets" 11
-                get_data_line "$line" "icmp_bytes" 12
+                        get_data_line "$line" "icmp_packets" 11
+                        get_data_line "$line" "icmp_bytes" 12
 
-                get_data_line "$line" "ipsec_packets" 13
-                get_data_line "$line" "ipsec_bytes" 14
+                        get_data_line "$line" "ipsec_packets" 13
+                        get_data_line "$line" "ipsec_bytes" 14
 
-                get_data_line "$line" "other_packets" 15
-                get_data_line "$line" "other_bytes" 16
-
-                send_buffer
-        fi
+                        get_data_line "$line" "other_packets" 15
+                        get_data_line "$line" "other_bytes"  16
+                fi
+                read line
+                s=$?
+                if [[ $s != 0 ]]; then
+                        run=1
+                fi
+        done | /usr/bin/zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -vv -i -
 done
