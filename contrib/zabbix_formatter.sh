3df0068d80cb1efe737e1617601fb6e418a349dc
splitice <mat999@gmail.com>
2015-10-21 21:49:08 +1100

    zabbix formatter performance tweaking

--- contrib/zabbix_formatter.sh
+++ contrib/zabbix_formatter.sh
@@ -1,38 +1,26 @@
 #!/bin/bash
-#Usage: ipstats eth1 | bash contrib/zabbix_formatter.sh SERVER | zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -i -
+#Usage: ipstats eth1 | bash contrib/zabbix_formatter.sh SERVER
 
 HOST="$1"
 EXIT=0
 
 function get_data_line {
-	echo "$1" | awk '{print "'$HOST' net.ipstat[\""$1"\",\""$2"\",\"'$2'\"] "$'$3'}' | cat
-	EXIT=$?
+        code=""
+        for ((i=2; i<${#@}; i+=2)); do
+                PROPERTY="${!i}"
+                let f=i+1
+                VALUE="${!f}"
+                code=$code'print "'$HOST' net.ipstat[\""$1"\",\""$2"\",\"'$PROPERTY'\"] "$'$VALUE';'
+        done
+        echo "$1" | awk '{'"$code"'}'
+        EXIT=$?
 }
 
 while read line; do
 	run=1
 	while [[ $run != 0 ]]; do
 		if [[ "${line:0:1}" != "#" ]]; then
-			get_data_line "$line" "tcp_packets" 3
-			get_data_line "$line" "tcp_bytes" 4
-
-			get_data_line "$line" "udp_packets" 5
-			get_data_line "$line" "udp_bytes" 6
-
-			get_data_line "$line" "gre_packets" 7
-			get_data_line "$line" "gre_bytes" 8
-
-			get_data_line "$line" "ipip_packets" 9
-			get_data_line "$line" "ipip_bytes" 10
-
-			get_data_line "$line" "icmp_packets" 11
-			get_data_line "$line" "icmp_bytes" 12
-
-			get_data_line "$line" "ipsec_packets" 13
-			get_data_line "$line" "ipsec_bytes" 14
-
-			get_data_line "$line" "other_packets" 15
-			get_data_line "$line" "other_bytes"  16
+			get_data_line "$line" "tcp_packets" 3 "tcp_bytes" 4 "udp_packets" 5 "udp_bytes" 6 "gre_packets" 7 "gre_bytes" 8 "ipip_packets" 9 "ipip_bytes" 10 "icmp_packets" 11 "icmp_bytes" 12 "ipsec_packets" 13 "ipsec_bytes" 14 "other_packets" 15 "other_bytes"  16
 		fi
 		if [[ $EXIT != 0 ]]; then
 			run=0
